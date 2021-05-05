ec8eb322bf15b9896e1ef42011394dd4da264a0c
Dennis Stengele <dennis@stengele.me>
2021-05-05 11:09:07 +0200

    Remove trailing comma

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["lazy", "requests",],
+    install_requires=["lazy", "requests"],
 )
